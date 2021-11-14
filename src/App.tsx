0caa5d7c591d05c2d769a5701ac39ab425ed774c
Giuliano Furgol <giulianofurgol@hotmail.com>
2021-11-14 16:25:02 +0100

    Add math.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import { create, all } from 'mathjs'
 import './App.css';
 
+const config = {}
+const math = create(all, config)
+
 function App() {
+  console.log(math.evaluate('12 / (2.3 + 0.7)')) // 4
+  console.log(math.evaluate('12 / 2 * 3')) // 18
+  console.log(math.evaluate('1 + 2 * 3')) // 7
+  console.log(math.evaluate('(1 + 2) * 3')) // 9
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Calculator</h1>
   );
 }
 
